2cccdc21e3949b9d5c0b7cbb4cbaa75f55c4593f
phelgo <phelgo@gmail.com>
2015-01-25 21:43:35 +0100

    Initial commit

---
+++ Vector3D.swift
@@ -0,0 +1,12 @@
+
+struct Vector3D {
+    let x: Double
+    let y: Double
+    let z: Double
+    
+    init(_ x: Double, _ y: Double, _ z: Double) {
+        self.x = x
+        self.y = y
+        self.z = z
+    }
+}
