38f8fb946c6e8b6f6a26e03799947aabcab704be
phelgo <phelgo@gmail.com>
2015-02-16 22:56:28 +0100

    Update access control on Vector4D

--- Vectors/Vector4D.swift
+++ Vectors/Vector4D.swift
@@ -3,68 +3,68 @@
 
 import SceneKit
 
-struct Vector4D: DebugPrintable, Equatable, Hashable, Printable {
-    let x: Double
-    let y: Double
-    let z: Double
-    let w: Double
+public struct Vector4D: DebugPrintable, Equatable, Hashable, Printable {
+    public let x: Double
+    public let y: Double
+    public let z: Double
+    public let w: Double
     
-    init(_ x: Double, _ y: Double, _ z: Double, _ w: Double) {
+    public init(_ x: Double, _ y: Double, _ z: Double, _ w: Double) {
         self.x = x
         self.y = y
         self.z = z
         self.w = w
     }
     
-    var magnitude: Double {
+    public var magnitude: Double {
         return sqrt(x * x + y * y + z * z + w * w)
     }
     
-    var normalized: Vector4D {
+    public var normalized: Vector4D {
         return Vector4D(x / magnitude, y / magnitude, z / magnitude, w / magnitude)
     }
     
-    var description: String {
+    public var description: String {
         return "Vector4D(\(x), \(y), \(z), \(w))"
     }
     
-    var debugDescription: String {
+    public var debugDescription: String {
         return "Vector4D(\(x), \(y), \(z), \(w)), magnitude: \(magnitude)"
     }
     
-    var hashValue: Int {
+    public var hashValue: Int {
         return x.hashValue ^ y.hashValue ^ z.hashValue ^ w.hashValue
     }
     
-    static let zero = Vector4D(0, 0, 0, 0)
-    static let one = Vector4D(1, 1, 1, 1)
+    public static let zero = Vector4D(0, 0, 0, 0)
+    public static let one = Vector4D(1, 1, 1, 1)
 }
 
-func == (left: Vector4D, right: Vector4D) -> Bool {
+public func == (left: Vector4D, right: Vector4D) -> Bool {
     return (left.x == right.x) && (left.y == right.y) && (left.z == right.z) && (left.w == right.w)
 }
 
-func + (left: Vector4D, right: Vector4D) -> Vector4D {
+public func + (left: Vector4D, right: Vector4D) -> Vector4D {
     return Vector4D(left.x + right.x, left.y + right.y, left.z + right.z, left.w + right.w)
 }
 
-func - (left: Vector4D, right: Vector4D) -> Vector4D {
+public func - (left: Vector4D, right: Vector4D) -> Vector4D {
     return Vector4D(left.x - right.x, left.y - right.y, left.z - right.z, left.w - right.w)
 }
 
-func += (inout left: Vector4D, right: Vector4D) {
+public func += (inout left: Vector4D, right: Vector4D) {
     left = left + right
 }
 
-func -= (inout left: Vector4D, right: Vector4D) {
+public func -= (inout left: Vector4D, right: Vector4D) {
     left = left - right
 }
 
-prefix func - (vector: Vector4D) -> Vector4D {
+public prefix func - (vector: Vector4D) -> Vector4D {
     return Vector4D(-vector.x, -vector.y, -vector.z, -vector.w)
 }
 
-extension SCNVector4 {
+public extension SCNVector4 {
     init (_ value: Vector4D) {
         self.x = Float(value.x)
         self.y = Float(value.y)
@@ -73,6 +73,6 @@ extension SCNVector4 {
     }
 }
 
-prefix func ~ (vector: Vector4D) -> SCNVector4 {
+public prefix func ~ (vector: Vector4D) -> SCNVector4 {
     return SCNVector4(vector)
 }
